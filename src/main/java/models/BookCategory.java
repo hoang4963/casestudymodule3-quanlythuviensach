d2cb83b6bfb360cc67c0c11208747979699bf7c8
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-10-06 00:28:43 +0700

    class book category

---
+++ src/main/java/models/BookCategory.java
@@ -0,0 +1,41 @@
+package models;
+
+public class BookCategory {
+    protected int id;
+    protected String bookCategoryId;
+    protected String bookCategoryName;
+
+
+    public BookCategory() {
+    }
+
+    public BookCategory(int id,String bookCategoryId, String bookCategoryName) {
+        this.id = id;
+        this.bookCategoryId = bookCategoryId;
+        this.bookCategoryName = bookCategoryName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBookCategoryId() {
+        return bookCategoryId;
+    }
+
+    public void setBookCategoryId(String bookCategoryId) {
+        this.bookCategoryId = bookCategoryId;
+    }
+
+    public String getBookCategoryName() {
+        return bookCategoryName;
+    }
+
+    public void setBookCategoryName(String bookCategoryName) {
+        this.bookCategoryName = bookCategoryName;
+    }
+}
