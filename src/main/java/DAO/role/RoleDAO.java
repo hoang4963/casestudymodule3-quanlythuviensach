5d7f8fa3e5698e52f070c2bc009f34b9a8060c49
hoang4963 <hoangth.finbox@gmail.com>
2022-10-05 16:49:42 +0700

    fix roledao

--- src/main/java/DAO/role/RoleDAO.java
+++ src/main/java/DAO/role/RoleDAO.java
@@ -8,9 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RoleDAO implements IRoleDAO {
-    private final String jdbcURL ="jdbc:mysql://127.0.0.1:3306/quanlythuviensach?useSSL=false";
-    private final String jdbcRolename = "root";
-    private final String jdbcPassword = "Nhth1319.";
 
     private static final String INSERT_ROLE_SQL = "INSERT INTO role (roleId,name) VALUES (?,?);";
     private static final String SELECT_ROLE_BY_ID = "select * from role where id =?";
