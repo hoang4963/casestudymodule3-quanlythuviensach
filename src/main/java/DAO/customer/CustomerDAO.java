1a77e0829df99ecf195639ca511379815f6473b0
Duonghieu97st <Duonghieu97st@gmail.com>
2022-10-10 10:18:28 +0700

    update add customer

--- src/main/java/DAO/customer/CustomerDAO.java
+++ src/main/java/DAO/customer/CustomerDAO.java
@@ -7,13 +7,12 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CustomerDAO implements ICustomerDAO{
-    private static final String INSERT_CUSTOMER_SQL = "INSERT INTO customer (customerId,name,birthday,email,phone,avatar,password,role_id) VALUES (?,?,?,?,?,?,?);";
+public class CustomerDAO implements ICustomerDAO {
+    private static final String INSERT_CUSTOMER_SQL = "INSERT INTO customer (customerId,name,email,role_id,password) VALUES (?,?,?,?,?);";
     private static final String SELECT_CUSTOMER_BY_ID = "select * from customer where id =?";
     private static final String SELECT_ALL_CUSTOMERS = "select * from customer";
     private static final String DELETE_CUSTOMERS_SQL = "delete from customer where id = ?;";
     private static final String UPDATE_CUSTOMERS_SQL = "update customer set customerId = ?, name = ?, birthday = ?, email = ?, phone = ?, avatar = ?,password =?, role_id = ? where id = ?;";
-
 
 
     public CustomerDAO() {
@@ -25,16 +24,13 @@ public class CustomerDAO implements ICustomerDAO{
         try (Connection connection = ConnectionDB.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_CUSTOMER_SQL)) {
             preparedStatement.setString(1, customer.getCustomerId());
             preparedStatement.setString(2, customer.getCustomerName());
-            preparedStatement.setDate(3,customer.getCustomerBirthday());
-            preparedStatement.setString(4,customer.getCustomerEmail());
-            preparedStatement.setString(5,customer.getCustomerPhone());
-            preparedStatement.setString(6,customer.getCustomerAvatar());
-            preparedStatement.setString(7,customer.getCustomerPassword());
-            preparedStatement.setString(8,customer.getCustomerRoleId());
+            preparedStatement.setString(3, customer.getCustomerEmail());
+            preparedStatement.setString(4, customer.getCustomerPassword());
+            preparedStatement.setString(5, customer.getCustomerRoleId());
+
             System.out.println(preparedStatement);
             preparedStatement.executeUpdate();
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -44,10 +40,10 @@ public class CustomerDAO implements ICustomerDAO{
         Customer customer = null;
         try (Connection connection = ConnectionDB.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(SELECT_CUSTOMER_BY_ID)) {
-            preparedStatement.setInt(1,id);
+            preparedStatement.setInt(1, id);
             System.out.println(preparedStatement);
             ResultSet rs = preparedStatement.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 String customerId = rs.getString("customerId");
                 String customerName = rs.getString("name");
                 Date birthday = rs.getDate("birthday");
@@ -56,9 +52,9 @@ public class CustomerDAO implements ICustomerDAO{
                 String avatar = rs.getString("avatar");
                 String roleid = rs.getString("role_id");
                 String password = rs.getString("password");
-                customer = new Customer(id,customerId,customerName,birthday,email,phone,avatar,roleid,password);
+                customer = new Customer(id, customerId, customerName, birthday, email, phone, avatar, roleid, password);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return customer;
@@ -67,11 +63,11 @@ public class CustomerDAO implements ICustomerDAO{
     @Override
     public List<Customer> selectAllCustomers() {
         List<Customer> customers = new ArrayList<>();
-        try(Connection connection = ConnectionDB.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_CUSTOMERS)){
+        try (Connection connection = ConnectionDB.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_CUSTOMERS)) {
             System.out.println(preparedStatement);
             ResultSet rs = preparedStatement.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 int id = rs.getInt("id");
                 String customerId = rs.getString("customerId");
                 String name = rs.getString("name");
@@ -81,9 +77,9 @@ public class CustomerDAO implements ICustomerDAO{
                 String avatar = rs.getString("avatar");
                 String roleid = rs.getString("role_id");
                 String password = rs.getString("password");
-                customers.add(new Customer(id,customerId,name,birthday,email,phone,avatar,roleid,password));
+                customers.add(new Customer(id, customerId, name, birthday, email, phone, avatar, roleid, password));
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return customers;
@@ -92,11 +88,11 @@ public class CustomerDAO implements ICustomerDAO{
     @Override
     public boolean deleteCustomer(int id) throws SQLException {
         boolean rowDeleted;
-        try(Connection connection = ConnectionDB.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_CUSTOMERS_SQL)){
+        try (Connection connection = ConnectionDB.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_CUSTOMERS_SQL)) {
             System.out.println(preparedStatement);
-            preparedStatement.setInt(1,id);
-            rowDeleted = preparedStatement.executeUpdate() >0;
+            preparedStatement.setInt(1, id);
+            rowDeleted = preparedStatement.executeUpdate() > 0;
         }
         return rowDeleted;
     }
@@ -104,18 +100,18 @@ public class CustomerDAO implements ICustomerDAO{
     @Override
     public boolean updateCustomer(Customer customer) throws SQLException {
         boolean rowUpdated;
-        try(Connection connection = ConnectionDB.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_CUSTOMERS_SQL)){
-            preparedStatement.setString(1,customer.getCustomerId());
-            preparedStatement.setString(2,customer.getCustomerName());
-            preparedStatement.setDate(3,customer.getCustomerBirthday());
-            preparedStatement.setString(4,customer.getCustomerEmail());
-            preparedStatement.setString(5,customer.getCustomerPhone());
-            preparedStatement.setString(6,customer.getCustomerAvatar());
-            preparedStatement.setString(7,customer.getCustomerPassword());
-            preparedStatement.setString(8,customer.getCustomerRoleId());
-            preparedStatement.setInt(9,customer.getId());
-            rowUpdated = preparedStatement.executeUpdate() >0;
+        try (Connection connection = ConnectionDB.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_CUSTOMERS_SQL)) {
+            preparedStatement.setString(1, customer.getCustomerId());
+            preparedStatement.setString(2, customer.getCustomerName());
+            preparedStatement.setDate(3, customer.getCustomerBirthday());
+            preparedStatement.setString(4, customer.getCustomerEmail());
+            preparedStatement.setString(5, customer.getCustomerPhone());
+            preparedStatement.setString(6, customer.getCustomerAvatar());
+            preparedStatement.setString(7, customer.getCustomerPassword());
+            preparedStatement.setString(8, customer.getCustomerRoleId());
+            preparedStatement.setInt(9, customer.getId());
+            rowUpdated = preparedStatement.executeUpdate() > 0;
         }
         return rowUpdated;
     }
