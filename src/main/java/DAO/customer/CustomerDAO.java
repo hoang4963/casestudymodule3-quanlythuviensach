8d4020545c8a480fe41b04bdd948b3b2fdd2eed8
Duonghieu97st <Duonghieu97st@gmail.com>
2022-10-10 10:23:48 +0700

    update customer

--- src/main/java/DAO/customer/CustomerDAO.java
+++ src/main/java/DAO/customer/CustomerDAO.java
@@ -8,29 +8,17 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-<<<<<<< HEAD
-public class CustomerDAO implements ICustomerDAO{
-    private static final String INSERT_CUSTOMER_SQL = "INSERT INTO customer (customerId,name,birthday,email,phone,avatar,password,role_id) VALUES (?,?,?,?,?,?,?,?);";
-    private static final String SELECT_CUSTOMER_BY_ID = "select * from customer where id = ?";
-    private static final String SELECT_ALL_CUSTOMERS = "select * from customer where delete_status = 0";
-    private static final String DELETE_CUSTOMERS_SQL = "update customer set delete_status = 1 where id = ?";
-    private static final String UPDATE_CUSTOMERS_SQL = "update customer set customerId = ?, name = ?, birthday = ?, email = ?, phone = ?, avatar = ?, role_id = ? where id = ?;";
-=======
 public class CustomerDAO implements ICustomerDAO {
     private static final String INSERT_CUSTOMER_SQL = "INSERT INTO customer (customerId,name,email,role_id,password) VALUES (?,?,?,?,?);";
     private static final String SELECT_CUSTOMER_BY_ID = "select * from customer where id =?";
     private static final String SELECT_ALL_CUSTOMERS = "select * from customer";
     private static final String DELETE_CUSTOMERS_SQL = "delete from customer where id = ?;";
     private static final String UPDATE_CUSTOMERS_SQL = "update customer set customerId = ?, name = ?, birthday = ?, email = ?, phone = ?, avatar = ?,password =?, role_id = ? where id = ?;";
->>>>>>> hieu
 
     private static final String SEARCH_CUSTOMERS_BY_NAME = "select * from customer where customer.name like ?;";
 
-<<<<<<< HEAD
     private static final String RETURN_CUSTOMERS = "update customer set delete_status = 0 where id = ?";
 
-=======
->>>>>>> hieu
     public CustomerDAO() {
     }
 
@@ -142,7 +130,6 @@ public class CustomerDAO implements ICustomerDAO {
     @Override
     public boolean updateCustomer(Customer customer) throws SQLException {
         boolean rowUpdated;
-<<<<<<< HEAD
         try(Connection connection = ConnectionDB.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_CUSTOMERS_SQL)){
             preparedStatement.setString(1,customer.getCustomerId());
@@ -154,20 +141,7 @@ public class CustomerDAO implements ICustomerDAO {
             preparedStatement.setString(7,customer.getCustomerRoleId());
             preparedStatement.setInt(8,customer.getId());
             rowUpdated = preparedStatement.executeUpdate() >0;
-=======
-        try (Connection connection = ConnectionDB.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_CUSTOMERS_SQL)) {
-            preparedStatement.setString(1, customer.getCustomerId());
-            preparedStatement.setString(2, customer.getCustomerName());
-            preparedStatement.setDate(3, customer.getCustomerBirthday());
-            preparedStatement.setString(4, customer.getCustomerEmail());
-            preparedStatement.setString(5, customer.getCustomerPhone());
-            preparedStatement.setString(6, customer.getCustomerAvatar());
-            preparedStatement.setString(7, customer.getCustomerPassword());
-            preparedStatement.setString(8, customer.getCustomerRoleId());
-            preparedStatement.setInt(9, customer.getId());
-            rowUpdated = preparedStatement.executeUpdate() > 0;
->>>>>>> hieu
+
         }
         return rowUpdated;
     }
